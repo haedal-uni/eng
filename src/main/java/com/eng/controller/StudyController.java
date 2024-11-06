f50563ff2718d129568d9310ba68e15f28be3d58
soyeon <luora@naver.com>
2024-11-07 03:06:31 +0900

    [update] query 및 return값 수정 [#6]

--- src/main/java/com/eng/controller/StudyController.java
+++ src/main/java/com/eng/controller/StudyController.java
@@ -3,7 +3,9 @@ package com.eng.controller;
 import com.eng.dto.StudyResponseDto;
 import com.eng.service.StudyService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -13,8 +15,8 @@ import java.util.List;
 public class StudyController {
     private final StudyService service;
 
-    @GetMapping("/study-words")
-    public List<StudyResponseDto> getStudyWords(String username){
+    @GetMapping("/study-words/{username}")
+    public Page<StudyResponseDto> getStudyWords(@PathVariable String username){
         return service.getStudyWord(username);
     }
 }
--- src/main/java/com/eng/repository/MeanRepository.java
+++ src/main/java/com/eng/repository/MeanRepository.java
@@ -1,6 +1,8 @@
 package com.eng.repository;
 
 import com.eng.domain.Meaning;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -15,13 +17,8 @@ public interface MeanRepository extends JpaRepository<Meaning, Long> {
     @Query(nativeQuery = true, value = "SELECT * FROM Meaning m WHERE m.word_id = :wordId AND m.meaning = :meaning LIMIT 1")
     Optional<Meaning> findByMean(Long wordId, String meaning);
 
-    @Query(nativeQuery = true, value =
-            "SELECT m.*, s.* " +
-                    "FROM meaning m " +
-                    "INNER JOIN sentence s ON m.id = s.meaning_id " +
-                    "WHERE m.id NOT IN ( " +
-                    "    SELECT study.meaning_id FROM study WHERE study.user_id = :userId " +
-                    ") " +
-                    "ORDER BY m.id LIMIT 10")
-    List<Object[]> findByMeanForStudyWithSentence(Long userId);
+    @Query("SELECT m, s FROM Meaning m JOIN Sentence s ON m.id = s.meaning.id " +
+            "WHERE m.id NOT IN (SELECT study.meaning.id FROM Study study WHERE study.user.id = :userId) " +
+            "ORDER BY m.id")
+    Page<Object[]> findByMeanForStudyWithSentence(Long userId, Pageable pageable);
 }
