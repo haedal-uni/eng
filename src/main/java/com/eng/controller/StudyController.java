9e81fb6f6d2b80a0a9484bef6e082d3f7ba0c742
soyeon <luora@naver.com>
2024-11-06 23:06:06 +0900

    [add] controller 추가 [#6]

--- src/main/java/com/eng/controller/StudyController.java
+++ src/main/java/com/eng/controller/StudyController.java
@@ -1,8 +1,20 @@
 package com.eng.controller;
 
+import com.eng.dto.StudyResponseDto;
+import com.eng.service.StudyService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class StudyController {
+import java.util.List;
 
+@RestController
+@RequiredArgsConstructor
+public class StudyController {
+    private final StudyService service;
+
+    @GetMapping("/study-words")
+    public List<StudyResponseDto> getStudyWords(String username){
+        return service.getStudyWord(username);
+    }
 }
