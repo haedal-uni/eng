7aa49d1311c6d3011d675a347dfd31a970594431
soyeon <luora@naver.com>
2024-11-06 23:03:09 +0900

    [add] service 및 repository 추가 [#6]

--- src/main/java/com/eng/repository/MeanRepository.java
+++ src/main/java/com/eng/repository/MeanRepository.java
@@ -4,6 +4,7 @@ import com.eng.domain.Meaning;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MeanRepository extends JpaRepository<Meaning, Long> {
@@ -13,4 +14,14 @@ public interface MeanRepository extends JpaRepository<Meaning, Long> {
 
     @Query(nativeQuery = true, value = "SELECT * FROM Meaning m WHERE m.word_id = :wordId AND m.meaning = :meaning LIMIT 1")
     Optional<Meaning> findByMean(Long wordId, String meaning);
+
+    @Query(nativeQuery = true, value =
+            "SELECT m.*, s.* " +
+                    "FROM meaning m " +
+                    "INNER JOIN sentence s ON m.id = s.meaning_id " +
+                    "WHERE m.id NOT IN ( " +
+                    "    SELECT study.meaning_id FROM study WHERE study.user_id = :userId " +
+                    ") " +
+                    "ORDER BY m.id LIMIT 10")
+    List<Object[]> findByMeanForStudyWithSentence(Long userId);
 }
--- src/main/java/com/eng/service/StudyService.java
+++ src/main/java/com/eng/service/StudyService.java
@@ -1,11 +1,12 @@
 package com.eng.service;
 
-import com.eng.domain.*;
+import com.eng.domain.Meaning;
+import com.eng.domain.Sentence;
+import com.eng.domain.User;
 import com.eng.dto.StudyResponseDto;
 import com.eng.exception.notfound.UserNotFoundException;
-import com.eng.exception.notfound.WordNotFoundException;
-import com.eng.repository.*;
-import jakarta.transaction.Transactional;
+import com.eng.repository.MeanRepository;
+import com.eng.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,21 +16,9 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class StudyService {
-    private final StudyRepository studyRepository;
-    private final WordRepository wordRepository;
     private final UserRepository userRepository;
     private final MeanRepository meanRepository;
 
-    /*
-    단어 학습하기 10개씩 보여주기
-
-    단어를 어떻게 학습할 것인가?
-    사용자가 이전에 공부했던 것은 제외해야함
-    테이블 생성
-    Study : 사용자가 공부한 word_id 목록, user_id
-    Wrong : 사용자가 퀴즈를 풀어서 틀린 word_id 목록, user_id
-    User : 사용자 정보
-     */
     // 단어 가져오기
     public List<StudyResponseDto> getStudyWord(String username){
         User user = userRepository.findByUsername(username).orElseThrow(UserNotFoundException::new);
@@ -47,15 +36,5 @@ public class StudyService {
             ));
         }
         return list;
-    }
-
-    // 단어 저장하기
-    @Transactional
-    public void saveStudyWord(Long wordId, String username, String meaning){
-        Word word = wordRepository.findById(wordId).orElseThrow(WordNotFoundException::new);
-        User user = userRepository.findByUsername(username).orElseThrow(UserNotFoundException::new);
-        Meaning mean = meanRepository.findByMean(wordId, meaning).orElseThrow();
-        Study study = Study.createStudy(user, word, mean);
-        studyRepository.save(study);
     }
 }
