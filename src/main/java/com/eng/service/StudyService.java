ccc2f35d5fea550728deb4f9e44cd7045b6f55fa
soyeon <luora@naver.com>
2024-11-07 17:46:24 +0900

    [add] 페이징 추가 [#6]

--- src/main/java/com/eng/service/StudyService.java
+++ src/main/java/com/eng/service/StudyService.java
@@ -10,7 +10,6 @@ import com.eng.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
