3683defd9f8acd046f1003ae914afe2c512c88a2
soyeon <luora@naver.com>
2024-11-22 17:42:00 +0900

    [add] 단어에 따른 예문 개수 여러개 [#13]

--- src/main/java/com/eng/repository/SentenceRepository.java
+++ src/main/java/com/eng/repository/SentenceRepository.java
@@ -4,11 +4,13 @@ import com.eng.domain.Sentence;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface SentenceRepository extends JpaRepository<Sentence, Long> {
     @Query(nativeQuery = true, value = "SELECT EXISTS " +
             "(SELECT 1 FROM Sentence s WHERE s.meaning_id = :meaningId AND s.sentence = :sentence)")
     int existsByMeanAndSentence(Long meaningId, String sentence);
 
     @Query("select st from Sentence st where st.meaning.id = :meaningId")
-    Sentence findBySentence(Long meaningId);
+    List<Sentence> findBySentence(Long meaningId);
 }
